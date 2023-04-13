db4d25fdbdd397c78aea8700b0e42a80c1de4d06
陈亿辛 <327527333@qq.com>
2023-04-13 09:49:23 +0800

    用户注册接口

--- user-web/api/user.go
+++ user-web/api/user.go
@@ -6,6 +6,7 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"github.com/go-redis/redis/v8"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -62,6 +63,10 @@ func HandleGrpcErrorToHttp(err error, c *gin.Context) {
 			case codes.Unavailable:
 				c.JSON(http.StatusInternalServerError, gin.H{
 					"msg": "用户服务不可用",
+				})
+			case codes.AlreadyExists:
+				c.JSON(http.StatusNotFound, gin.H{
+					"msg": "用户已存在",
 				})
 			default:
 				c.JSON(http.StatusInternalServerError, gin.H{
@@ -213,4 +218,81 @@ func PassWordLogin(c *gin.Context) {
 			}
 		}
 	}
+}
+
+func Register(c *gin.Context) {
+	//用户注册
+	registerFrom := forms.RegisterFrom{}
+	if err := c.ShouldBind(&registerFrom); err != nil {
+		HandleValidatorError(c, err)
+		return
+	}
+
+	//验证码校验
+	rdb := redis.NewClient(&redis.Options{
+		Addr: fmt.Sprintf("%s:%d", global.ServerConfig.RedisInfo.Host, global.ServerConfig.RedisInfo.Port),
+	})
+	value, err := rdb.Get(context.Background(), registerFrom.Mobile).Result()
+	if err == redis.Nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code": "验证码错误",
+		})
+		return
+	} else {
+		if value != registerFrom.Code {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"code": "验证码错误",
+			})
+			return
+		}
+	}
+
+	//拨号连接用户grpc服务
+	userConn, err := grpc.Dial(fmt.Sprintf("%s:%d", global.ServerConfig.UserSrvInfo.Host, global.ServerConfig.UserSrvInfo.Port), grpc.WithInsecure())
+	if err != nil {
+		zap.S().Errorw("[GetUserList] 连接 【用户服务失败】",
+			"msg", err.Error(),
+		)
+	}
+
+	//生成grpc的client并调用接口
+	userSrvClient := proto.NewUserClient(userConn)
+
+	user, err := userSrvClient.CreateUser(context.Background(), &proto.CreateUserInfo{
+		NickName: registerFrom.Mobile,
+		Password: registerFrom.PassWord,
+		Mobile:   registerFrom.Mobile,
+	})
+
+	if err != nil {
+		zap.S().Errorf("[Register] 新建 [新建用户失败] 失败: %s", err.Error())
+		HandleGrpcErrorToHttp(err, c)
+		return
+	}
+
+	j := middlewares.NewJWT()
+	claims := models.CustomClaims{
+		ID:          uint(user.Id),
+		NickName:    user.NickName,
+		AuthorityId: uint(user.Role),
+		StandardClaims: jwt.StandardClaims{
+			NotBefore: time.Now().Unix(),               //签名的生效时间
+			ExpiresAt: time.Now().Unix() + 60*60*24*30, //30天过期
+			Issuer:    "imooc",
+		},
+	}
+	token, err := j.CreateToken(claims)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, map[string]string{
+			"msg": "生成token失败",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"id":         user.Id,
+		"nick_name":  user.NickName,
+		"token":      token,
+		"expired_at": (time.Now().Unix() + 60*60*24*30) * 1000,
+	})
 }
--- user-web/forms/user.go
+++ user-web/forms/user.go
@@ -6,3 +6,9 @@ type PassWordLoginForm struct {
 	Captcha   string `form:"captcha" json:"captcha" binding:"required,min=5,max=5"`
 	CaptchaId string `form:"captcha_id" json:"captcha_id" binding:"required"`
 }
+
+type RegisterFrom struct {
+	Mobile   string `form:"mobile" json:"mobile" binding:"required,mobile"`
+	PassWord string `form:"pass_word" json:"pass_word" binding:"required,min=3,max=20"`
+	Code     string `form:"code" json:"code" binding:"required,min=6,max=6"`
+}
