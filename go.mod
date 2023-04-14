42c45d05df0f048aff0e690653711a81fc4d3386
陈亿辛 <327527333@qq.com>
2023-04-14 11:03:54 +0800

    gin集成nacos

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/hashicorp/consul/api v1.20.0
 	github.com/mbobakov/grpc-consul-resolver v1.5.2
 	github.com/mojocn/base64Captcha v1.3.5
+	github.com/nacos-group/nacos-sdk-go v1.1.4
 	github.com/spf13/viper v1.15.0
 	go.uber.org/zap v1.24.0
 	google.golang.org/grpc v1.54.0
@@ -32,6 +33,7 @@ require (
 	github.com/OpenPeeDeeP/depguard v1.1.1 // indirect
 	github.com/alexkohler/prealloc v1.0.0 // indirect
 	github.com/alingse/asasalint v0.0.11 // indirect
+	github.com/aliyun/alibaba-cloud-sdk-go v1.61.18 // indirect
 	github.com/armon/go-metrics v0.4.1 // indirect
 	github.com/ashanbrown/forbidigo v1.5.1 // indirect
 	github.com/ashanbrown/makezero v1.1.1 // indirect
@@ -41,6 +43,7 @@ require (
 	github.com/bombsimon/wsl/v3 v3.4.0 // indirect
 	github.com/breml/bidichk v0.2.4 // indirect
 	github.com/breml/errchkjson v0.3.1 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/butuzov/ireturn v0.1.1 // indirect
 	github.com/bytedance/sonic v1.8.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
@@ -60,6 +63,7 @@ require (
 	github.com/fzipp/gocyclo v0.6.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-critic/go-critic v0.7.0 // indirect
+	github.com/go-errors/errors v1.0.1 // indirect
 	github.com/go-playground/form v3.1.4+incompatible // indirect
 	github.com/go-toolsmith/astcast v1.1.0 // indirect
 	github.com/go-toolsmith/astcopy v1.1.0 // indirect
@@ -105,6 +109,7 @@ require (
 	github.com/jgautheron/goconst v1.5.1 // indirect
 	github.com/jingyugao/rowserrcheck v1.1.1 // indirect
 	github.com/jirfag/go-printf-func-name v0.0.0-20200119135958-7558a9eaa5af // indirect
+	github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af // indirect
 	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/julz/importas v0.1.0 // indirect
@@ -208,6 +213,7 @@ require (
 	golang.org/x/tools v0.7.0 // indirect
 	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	honnef.co/go/tools v0.4.3 // indirect
--- user-web/config/config.go
+++ user-web/config/config.go
@@ -1,37 +1,47 @@
 package config
 
 type UserSrvConfig struct {
-	Host string `mapstructure:"host"`
-	Port int    `mapstructure:"port"`
-	Name string `mapstructure:"name"`
+	Host string `mapstructure:"host" json:"host"`
+	Port int    `mapstructure:"port" json:"port"`
+	Name string `mapstructure:"name" json:"name"`
 }
 
 type JWTConfig struct {
-	SigningKey string `mapstructure:"key"`
+	SigningKey string `mapstructure:"key" json:"key"`
 }
 
 type AliSmsConfig struct {
-	ApiKey     string `mapstructure:"key"`
-	ApiSecrect string `mapstructure:"secrect"`
-	Expire     int    `mapstructure:"expire"`
-}
-
-type RedisConfig struct {
-	Host string `mapstructure:"host"`
-	Port int    `mapstructure:"port"`
+	ApiKey     string `mapstructure:"key" json:"key"`
+	ApiSecrect string `mapstructure:"secret" json:"secret"`
+	Expire     int    `mapstructure:"expire" json:"expire"`
 }
 
 type ConsulConfig struct {
-	Host string `mapstructure:"host"`
-	Port int    `mapstructure:"port"`
+	Host string `mapstructure:"host" json:"host"`
+	Port int    `mapstructure:"port" json:"port"`
+}
+
+type RedisConfig struct {
+	Host string `mapstructure:"host" json:"host"`
+	Port int    `mapstructure:"port" json:"port"`
 }
 
 type ServerConfig struct {
-	Name        string        `mapstructure:"name"`
-	Port        int           `mapstructure:"port"`
-	UserSrvInfo UserSrvConfig `mapstructure:"user_srv"`
-	JWTInfo     JWTConfig     `mapstructure:"jwt"`
-	AliSmsInfo  AliSmsConfig  `mapstructure:"sms"`
-	RedisInfo   RedisConfig   `mapstructure:"redis"`
-	ConsulInfo  ConsulConfig  `mapstructure:"consul"`
+	Name        string        `mapstructure:"name" json:"name"`
+	Port        int           `mapstructure:"port" json:"port"`
+	UserSrvInfo UserSrvConfig `mapstructure:"user_srv" json:"user_srv"`
+	JWTInfo     JWTConfig     `mapstructure:"jwt" json:"jwt"`
+	AliSmsInfo  AliSmsConfig  `mapstructure:"sms" json:"sms"`
+	RedisInfo   RedisConfig   `mapstructure:"redis" json:"redis"`
+	ConsulInfo  ConsulConfig  `mapstructure:"consul" json:"consul"`
+}
+
+type NacosConfig struct {
+	Host      string `mapstructure:"host"`
+	Port      uint64 `mapstructure:"port"`
+	NameSpace string `mapstructure:"namespace"`
+	User      string `mapstructure:"user"`
+	Password  string `mapstructure:"password"`
+	DataId    string `mapstructure:"dataid"`
+	Group     string `mapstructure:"group"`
 }
